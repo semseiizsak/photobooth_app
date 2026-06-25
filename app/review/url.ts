39034672eac97b619ac0ed500021c07ae947896b
semseiizsak <60150728+semseiizsak@users.noreply.github.com>
2026-06-25 08:10:35 -0700

    update review URL

--- app/review/url.ts
+++ app/review/url.ts
@@ -1,1 +1,1 @@
-export const REVIEW_URL = "https://g.page/r/CX-Zh5FPbiu-EBM/review";
+export const REVIEW_URL = "https://search.google.com/local/writereview?placeid=ChIJQ2KCvDPdQUcRfH-eFhpvCrs&source=g.page.m._&laa=merchant-review-solicitation";
